795f6be34c850d4490ba3fc5158edc8bafd77791
ManojSelenium <manojqa5006@gmail.com>
2016-06-06 06:24:16 +0530

    First Commit

---
+++ MavenJenkins/src/test/java/com/testcases/FirstTest.java
@@ -0,0 +1,25 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	
+	
+	@Test
+	public void tc_1() {
+		System.out.println("First Test Case");
+	}
+	
+	
+	
+	@Test
+	public void tc_2() {
+		System.out.println("Second Test Case");
+	}
+	
+	@Test
+	public void tc_3() {
+		System.out.println("Third Test Case");
+	}
+}
